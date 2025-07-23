cdc1ad2e3c985a8bb6b277f7d1f2ced1b1702c8b
thuydt <thuydt@canifa.com>
2025-07-23 20:18:03 +0700

    commit20250723

---
+++ project/td_module/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Project TD',
+    'version': '1.0',
+    'sequence': 1,
+    'category': 'TD',
+    'description': """
+        This module will install all module dependencies of td_module.""",
+    'depends': [
+        'base',
+        'contacts',        
+        'sale_management',
+        'maintenance',
+        'documents',
+        'hr',
+        ],
+    'summary': 'TD Project Module',
+    'website': 'https://thangmaytriduc.com',
+    'data': [
+        # SECURITY
+        # DATA
+        # Wizards
+        # VIEWS
+        # REPORT
+        # MENU
+        # MENU - GROUP
+        # POST OBJECT
+        # Template
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'assets': {
+    },
+}
