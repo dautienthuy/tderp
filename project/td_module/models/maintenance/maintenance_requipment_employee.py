2280a036d09a45d2ea288c352fb5a92f443313bd
dautienthuy <dautienthuy@gmail.com>
2025-08-05 21:08:51 +0700

    .

--- project/td_module/models/maintenance/maintenance_requipment_employee.py
+++ project/td_module/models/maintenance/maintenance_requipment_employee.py
@@ -5,7 +5,7 @@ from odoo import api, fields, models, tools
 
 class MaintenanceRequipmentEmployee(models.Model):
     _name = 'maintenance.requipment.employee'    
-    _description = 'maintenance_requipment_employee'
+    _description = 'Maintenance Equipment Employee'
 
     equipment_id = fields.Many2one('maintenance.equipment', string=u'Vật tư thay thế')
     start_date = fields.Date(string=u'Ngày bắt đầu')
