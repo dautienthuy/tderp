567ad8a425930ffe912b5502f84b025d7b618cf4
dautienthuy <dautienthuy@gmail.com>
2025-08-19 17:34:57 +0700

    .

--- project/td_module/models/maintenance/maintenance_detail.py
+++ project/td_module/models/maintenance/maintenance_detail.py
@@ -13,5 +13,10 @@ class MaintenanceDetail(models.Model):
     uom_id = fields.Many2one('uom.uom', 'Đơn vị')
     quantity = fields.Float(u'Số lượng')
     unit_price = fields.Float(u'Đơn giá')
-    amount = fields.Float(u'Thành tiền')
+    amount = fields.Float(u'Thành tiền', compute='_compute_amount', store=True)
     note = fields.Char(u'Ghi chú')
+
+    @api.depends('quantity', 'unit_price')
+    def _compute_amount(self):
+        for line in self:
+            line.theoritical_amount = line.quantity * line.unit_price
