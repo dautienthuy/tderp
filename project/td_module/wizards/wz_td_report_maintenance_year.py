3282705ae56d752567737f3a48f8ccd23a9513fa
dautienthuy <dautienthuy@gmail.com>
2025-09-12 01:01:51 +0700

    .

--- project/td_module/wizards/wz_td_report_maintenance_year.py
+++ project/td_module/wizards/wz_td_report_maintenance_year.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 
 from odoo import api, fields, models
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 
 class WzTdReportMaintenanceYear(models.TransientModel):
@@ -8,10 +10,20 @@ class WzTdReportMaintenanceYear(models.TransientModel):
     _description = "Wz Td Report Maintenance Year"
     _rec_name = "from_date"
 
-    from_date = fields.Date('Từ ngày')
-    to_date = fields.Date('Đến ngày')
+    year = fields.Integer(required=True, default=lambda self: fields.Date.today().year-1)
+    from_date = fields.Date('Từ ngày', store=True, readonly=False, compute='_compute_dates')
+    to_date = fields.Date('Đến ngày', store=True, readonly=False, compute='_compute_dates')
     maintenance_request_id = fields.Many2one('maintenance.request', 'Maintenance Request')
     detail_ids = fields.One2many("wz.td.report.maintenance.year.detail", "report_id", string="Chi tiết")
+
+    
+    @api.depends('year')
+    def _compute_dates(self):
+        for record in self:
+            record.update({
+                'from_date': date(record.year, 1, 1),
+                'to_date': date(record.year, 12, 1) + relativedelta(day=31),
+            })
 
 
 class WzTdReportMaintenanceYearDetail(models.TransientModel):
